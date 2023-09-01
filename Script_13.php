1ab9ea6207b1292e62eefbd1bdbb5ffb89340575
milanbhalodiya12 <143157260+milanbhalodiya12@users.noreply.github.com>
2023-09-01 07:50:31 +0530

    Add files via upload

---
+++ Script_13.php
@@ -0,0 +1,6 @@
+    <?php
+        $linkText = "Click here";
+        $linkUrl = "https://www.example.com";
+        $anchorTag = '<a href="' . $linkUrl . '">' . $linkText . '</a>';
+        echo $anchorTag;
+    ?>
---
+++ Script_14.php
@@ -0,0 +1,11 @@
+<?php
+    echo "<ul>\n";
+    echo "    line1\n";
+    echo "    <ul>\n";
+    echo "       line2\n";
+    echo "        <ul>\n";
+    echo "            line3\n";
+    echo "        </ul>\n";
+    echo "    </ul>\n";
+    echo "</ul>\n";
+?>
